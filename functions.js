f27ccab947c32e1331a51c38409de6c12e1600c9
Nick Martini <njmnbb@mail.missouri.edu>
2015-09-29 08:54:28 -0500

    Reuploading all files
    Cleaned up jQuery functions, added document.title functionality,
    created custom 404 page, added RewriteRule and RewriteCond to .htaccess
    file

--- functions.js
+++ functions.js
@@ -1,79 +1,45 @@
-//Function used to grab .txt file and put it into the content box
-function displayAbout() {
-    var divObj = document.getElementById('information');
-    var xmlHttp = new XMLHttpRequest();
-            
-    xmlHttp.onload = setTimeout(function() {
-        if(xmlHttp.status == 200) {
-            //Get XML document
-            var xmlDoc = xmlHttp.responseXML;
-                    
-            //Set div's inner HTML
-            divObj.innerHTML = "<p>" + xmlHttp.responseText + "</p>";
-        }
-    }, 500);
-            
-            //Opening XML document provided
-            xmlHttp.open("GET", "text/about.txt", true);
-            xmlHttp.send();  
-}
+$(document).ready( function() {
 
-//Function used to grab .txt file and put it into the content box
-function displayProjects() {
-    var divObj = document.getElementById('information');
-    var xmlHttp = new XMLHttpRequest();
-            
-    xmlHttp.onload = setTimeout(function() {
-        if(xmlHttp.status == 200) {
-            //Get XML document
-            var xmlDoc = xmlHttp.responseXML;
-                    
-            //Set div's inner HTML
-            divObj.innerHTML = "<p>" + xmlHttp.responseText + "</p>";
-        }
-    }, 500);
-            
-            //Opening XML document provided
-            xmlHttp.open("GET", "text/projects.txt", true);
-            xmlHttp.send();  
-}
+    //  Changing fade in/out properties //
+    //  depending on window size        //
 
-//Function used to grab .txt file and put it into the content box
-function displayExperience() {
-    var divObj = document.getElementById('information');
-    var xmlHttp = new XMLHttpRequest();
-            
-    xmlHttp.onload = setTimeout(function() {
-        if(xmlHttp.status == 200) {
-            //Get XML document
-            var xmlDoc = xmlHttp.responseXML;
-                    
-            //Set div's inner HTML
-            divObj.innerHTML = "<p>" + xmlHttp.responseText + "</p>";
-        }
-    }, 500);
-            
-            //Opening XML document provided
-            xmlHttp.open("GET", "text/experiences.txt", true);
-            xmlHttp.send();  
-}
+    // If browser is > 1000px, fade in/out
+    if($(window).width() > 1000) {
+        $("#headerBox img").fadeIn(1700);
+        $("#headerBox ul").delay(1000).fadeIn(1700);
+        $("#contentContainer").delay(2000).fadeIn(1700);
+    }
+    // Otherwise, remove the .hidden class
+    else {
+        $("#headerBox img").removeClass("hidden");
+        $("#headerBox ul").removeClass("hidden");
+        $("#contentContainer").removeClass("hidden");
+    }
 
-//Function used to grab .txt file and put it into the content box
-function displayContact() {
-    var divObj = document.getElementById('information');
-    var xmlHttp = new XMLHttpRequest();
-            
-    xmlHttp.onload = setTimeout(function() {
-        if(xmlHttp.status == 200) {
-            //Get XML document
-            var xmlDoc = xmlHttp.responseXML;
-                    
-            //Set div's inner HTML
-            divObj.innerHTML = "<p>" + xmlHttp.responseText + "</p>";
+    //  Fading in/out each tab and  //
+    //  loading its content         //
+
+    var activeWindow = "blah";
+
+    //When a tab is clicked, this function runs
+    $("#headerBox a").click(function() {
+
+        //Create a variable for the name of the clicked tab
+        var tabName = $(this).attr('id');
+
+        //If the clicked tab is already active, ignore this function
+        if (activeWindow != tabName) {
+
+            //Change activeWindow to tabName for next tab click, then change the <title> appropriately
+            activeWindow = tabName;
+            document.title = "Nick Martini - " + tabName.charAt(0).toUpperCase() + tabName.slice(1);
+
+            //Fade out information, load new content, then fade back in
+            $("#information").fadeOut(500);
+            setTimeout(function() {
+                $("#information").load("text/" + tabName + ".txt");
+            }, 500);
+            $("#information").delay(200).fadeIn(500);
         }
-    }, 500);
-            
-            //Opening XML document provided
-            xmlHttp.open("GET", "text/contactme.txt", true);
-            xmlHttp.send();  
-}
+    });           
+}); 
